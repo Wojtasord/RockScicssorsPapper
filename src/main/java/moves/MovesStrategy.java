4af3340879942da03c55a42ca664a1738201b9b9
Wojtek Henszke <wojciech.henszke@nokia.com>
2019-10-20 12:00:52 +0200

    reverting master branch to initial

--- src/main/java/moves/MovesStrategy.java
+++
@@ -1,8 +0,0 @@
-package moves;
-
-public interface MovesStrategy {
-    boolean isLoosingWith(MovesStrategy playerMove);
-    boolean isWinningWith(MovesStrategy playerMove);
-
-    ActionName getName();
-}
